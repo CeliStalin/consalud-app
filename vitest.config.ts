442f64a8fa1ef4490104a02ba142d155ed25d570
Stalin <stalin.celi@consalud.cl>
2025-10-09 12:30:18 -0300

    mejoras

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,46 +1,43 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./vitest.setupEnv.ts'],
-      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-      exclude: [
-        'node_modules',
-        'dist',
-        '.idea',
-        '.git',
-        '.cache'
-      ],
-      coverage: {
-        provider: 'v8',
-        reporter: ['text', 'json', 'html'],
-        exclude: [
-          'node_modules/',
-          'dist/',
-          '**/*.d.ts',
-          'test/',
-          '**/*.test.{js,jsx,ts,tsx}',
-          '**/*.spec.{js,jsx,ts,tsx}',
-          '**/types/',
-          '**/*.config.*',
-          '**/coverage/'
-        ]
-      }
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
     },
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, './src')
-      }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setupEnv.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: [
+      'node_modules',
+      'dist',
+      '.idea',
+      '.git',
+      '.cache'
+    ],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.d.ts',
+        'test/',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/*.spec.{js,jsx,ts,tsx}',
+        '**/types/',
+        '**/*.config.*',
+        '**/coverage/'
+      ]
     }
-  })
-); 
+  }
+});
